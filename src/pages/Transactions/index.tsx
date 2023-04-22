5db02a896d126caa5ef578bdb34e15ca95d4cac1
Thiago <thiagofreitascarneiro@hotmail.com>
2023-04-22 17:28:58 -0300

    styled Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
+import { Summary } from "../../components/Summary";
 import { Header } from "../../components/header";
 
 export function Transactions() {
     return (
         <div>
            < Header /> 
+           <Summary />
         </div>
     )
 }
