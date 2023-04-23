f0d86d27f2e2c46037322a252bfd1041298a042a
Thiago <thiagofreitascarneiro@hotmail.com>
2023-04-23 17:23:21 -0300

    styled pages finished

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTrasanctionButton } from "./styles";
-
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -8,7 +9,13 @@ export function Header() {
             <HeaderContent>
                 <img src={logoImg} alt=""/>
 
-                <NewTrasanctionButton>Nova Transação</NewTrasanctionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTrasanctionButton>Nova Transação</NewTrasanctionButton>
+                    </Dialog.Trigger>
+                
+                    <NewTransactionModal />    
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
